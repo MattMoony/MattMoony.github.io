4449655041a5c6baa9e00a188a1b05ea960c52aa
Matthias Monschein <monschein.matthias@gmail.com>
2020-07-26 21:50:14 +0200

    🚧 work on banner

--- src/components/MainHeader.tsx
+++ src/components/MainHeader.tsx
@@ -6,10 +6,36 @@ import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import gooseGIF from '../images/goose.gif';
 import wavesGIF from '../images/waves.gif';
 import seaGIF from '../images/sea.gif';
+import gooseJPG from '../images/goose.jpg';
 
-export default class MainHeader extends React.Component {
+interface MainHeaderProps {
+  children?: React.ReactElement|React.ReactElement[];
+}
+
+interface MainHeaderState {
+  banner: boolean;
+}
+
+export default class MainHeader extends React.Component<MainHeaderProps, MainHeaderState> {
+  private header: HTMLElement;
   private article: HTMLElement;
   private scroll: HTMLButtonElement;
+
+  public constructor (props) {
+    super(props);
+    this.state = {
+      banner: false,
+    };
+  }
+
+  public componentDidMount(): void {
+    window.onscroll = e => {
+      if (window.scrollY > window.scrollY + this.scroll.getBoundingClientRect().bottom)
+        this.setState({ banner: true, });
+      else
+        this.setState({ banner: false, });
+    };
+  }
 
   private onScroll (): void {
     this.article.scrollIntoView({
@@ -21,12 +47,18 @@ export default class MainHeader extends React.Component {
   public render (): React.ReactElement {
     return (
       <>
-        <header className={style.header}>
+        <header className={style.banner + " " + (this.state.banner ? style.visible : '')}>
+          <div>
+            <img src={gooseJPG} alt="Me - *technically not*" />
+            <h1>Matt</h1>
+          </div>
+        </header>
+        <div ref={e => this.header = e} className={style.header}>
           <h1>Matt</h1>
           <img className={style.goose} src={gooseGIF} alt="Swimming Goose - *honk*" />
           <img className={style.waves} src={wavesGIF} alt="Waves - *sploosh*" />
           <button onClick={this.onScroll.bind(this)} ref={e => this.scroll = e} className={style.down}><FontAwesomeIcon icon={faChevronDown} /></button>
-        </header>
+        </div>
         <article ref={e => this.article = e} className={style.article}>
           <img className={style.sea} src={seaGIF} alt="Underwater - *Under the seaaaa*" />
           <div className={style.content}>
---
+++ src/projects/convnet-mnist.md
@@ -0,0 +1,13 @@
+---
+slug: "/projects/convnet-mnist"
+date: "2020-07-20"
+title: "ConvNet - MNIST Dataset"
+desc: "Simple convolutional neural network (purely numpy) to classify the original MNIST dataset. My first project with a convnet. 🖼"
+ref: "MattMoony/convnet_mnist"
+---
+
+# ConvNet - MNIST Dataset
+
+## About
+
+This is supposed to be a little test project. I want to play around with convolutional layers, pooling layers, normalization strategies (dropout, batch normalization), training algorithms (Vanilla SGD, SGD w. Momentum, etc.) and much more.
